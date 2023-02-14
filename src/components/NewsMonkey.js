7931c9d433948a6a387132d26c9274dcf69b6ef4
Dvinod1 <107790735+Dvinod1@users.noreply.github.com>
2023-02-14 11:49:53 +0530

    loading bar added

---
+++ src/components/NewsMonkey.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class NewsMonkey extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+            <h1>Welcome to News Monkey app</h1>
+            <h2>Created By...Dvinod</h2>
+            <p>Here you can access multiple catogery news</p>
+
+        </div>
+      </div>
+    )
+  }
+}
